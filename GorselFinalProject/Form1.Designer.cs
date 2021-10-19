03ff1da632b84a2d47138fb950cdc8c633f4f24d
Doğukan Karasansar <dogukan_14072002@hotmail.com>
2021-10-18 22:22:38 -0700

    list view oluşturuldu kayıt ekleme formu eklendi içerik tamamlanmadı

--- GorselFinalProject/Form1.Designer.cs
+++ GorselFinalProject/Form1.Designer.cs
@@ -29,34 +29,73 @@ namespace GorselFinalProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.form2routebtn = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // form2routebtn
             // 
-            this.button1.Location = new System.Drawing.Point(293, 200);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 49);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.form2routebtn.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.form2routebtn.FlatAppearance.BorderColor = System.Drawing.Color.LightSeaGreen;
+            this.form2routebtn.FlatAppearance.BorderSize = 4;
+            this.form2routebtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Olive;
+            this.form2routebtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.form2routebtn.Font = new System.Drawing.Font("Arial Black", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.form2routebtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.form2routebtn.Location = new System.Drawing.Point(918, 133);
+            this.form2routebtn.Name = "form2routebtn";
+            this.form2routebtn.Size = new System.Drawing.Size(298, 65);
+            this.form2routebtn.TabIndex = 0;
+            this.form2routebtn.Text = "Kayıt Ekle";
+            this.form2routebtn.UseVisualStyleBackColor = false;
+            this.form2routebtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.listView1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(41, 133);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(807, 457);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(335, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(627, 74);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GYM KAYIT SİSTEMİ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1256, 636);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.form2routebtn);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button form2routebtn;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- GorselFinalProject/Form1.cs
+++ GorselFinalProject/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace GorselFinalProject
 {
     public partial class Form1 : Form
     {
+        ListViewItem list = new ListViewItem();
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,26 @@ namespace GorselFinalProject
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Columns.Add("TC KİMLİK NO", 125);
+            listView1.Columns.Add("AD VE SOYAD", 95);
+            listView1.Columns.Add("ADRES", 125);
+            listView1.Columns.Add("AKTİFLİK", 65);
+
+            /* DATALAR */
+            string[] defaultData1 = { "12345678910", "Doğukan Karasansar", "İstanbul / Gelişim Üniversitesi", "Aktif" };
+            string[] defaultData2 = { "12346622910", "Esra Aybar", "İstanbul / Gelişim Üniversitesi", "Aktif Değil" };
+            string[] defaultData3 = { "1234662210", "Damla Sekmen", "İstanbul / Gelişim Üniversitesi", "Aktif" };
+
+            listView1.Items.Add(new ListViewItem(defaultData1));
+            listView1.Items.Add(new ListViewItem(defaultData2));
+            listView1.Items.Add(new ListViewItem(defaultData3));
         }
     }
 }
